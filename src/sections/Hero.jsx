29ff4fd4602ea538e45936b18d1cdb33679d2779
Siraddeen <sircoil17@gmail.com>
2024-06-13 18:20:27 +0530

    small change

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,11 +10,11 @@ const Hero = () => {
     <section
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10
-      max-container"
+      max-container bg-gradient-to-t from-red-200 border-y-orange-400"
     >
       <div
         className="relative xl:w-2/5 flex flex-col justify-center items-start w-full
-      max-xl:padding-x pt-28"
+      max-xl:padding-x pt-28 bg-gradient-to-b from-white to-gray-300"
       >
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
